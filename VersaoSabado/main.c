73e8f0a3e1494dc24b5a4f64e82fab0ad2455b7e
RafaelLearth <113041643+RafaelLearth@users.noreply.github.com>
2023-10-15 01:20:00 -0300

    Add files via upload

---
+++ VersaoSabado/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "cabeca.h"
+ 
+int main(){
+    int escolha;
+    int RRN;
+    int n;
+    char arqCSV[20];
+    char arqBIN[20];
+    scanf("%d",&escolha);
+    //printf("escolha feita foi:%d\n",escolha); //REMOVER NO FUTURO
+    switch(escolha){
+        case 1:
+        //printf("Caso1\n"); //REMOVER NO FUTURO
+        scanf("%s",arqCSV);
+        scanf("%s",arqBIN);
+        createTable(arqCSV,arqBIN);
+        break;
+        case 2:
+        //printf("Caso2\n"); //REMOVER NO FUTURO
+        scanf("%s",arqBIN);
+        SelectFrom(arqBIN);
+        break;
+        case 3:
+        //printf("Caso3\n"); //REMOVER NO FUTURO
+        scanf("%s",arqBIN);
+        scanf("%d",&n);
+        SelecWhere(arqBIN,n);
+        break;
+        case 4:
+        //printf("Caso4\n"); //REMOVER NO FUTURO
+        scanf("%s",arqBIN);
+        scanf("%d",&RRN);
+        RecuperaDados(arqBIN,RRN);
+        break;
+    }
+}
