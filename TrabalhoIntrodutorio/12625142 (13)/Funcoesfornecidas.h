d0c58c16a14291c24bfef8bdb643d5589d73e738
Gabriela Passos de Andrade <gabipandrade@gmail.com>
2025-02-26 23:46:19 -0300

    add code

---
+++ TrabalhoIntrodutorio/12625142 (13)/Funcoesfornecidas.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCIONALIDADEFORNECIDA_H__
+#define __FUNCIONALIDADEFORNECIDA_H__
+
+#include "cabeca.h"
+//FuncoesFornecidas
+void scan_quote_string(char *str);
+void binarioNaTela(char *nomeArquivoBinario);
+void readline(char* string);
+#endif
