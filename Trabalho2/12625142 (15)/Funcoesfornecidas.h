08bc8b96ffb4c386483038fa2ef1cdfec4e23576
Gabriela Passos de Andrade <gabipandrade@gmail.com>
2025-02-27 22:09:06 -0300

    Delete Trabalho2/12625142 (15) directory

--- Trabalho2/12625142 (15)/Funcoesfornecidas.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FUNCIONALIDADEFORNECIDA_H__
-#define __FUNCIONALIDADEFORNECIDA_H__
-
-#include "cabeca.h"
-//FuncoesFornecidas
-void scan_quote_string(char *str);
-void binarioNaTela(char *nomeArquivoBinario);
-void readline(char* string);
-#endif
