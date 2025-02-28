92e100be5c7a3962fcd055d35ed5203afb08e31a
Gabriela Passos de Andrade <gabipandrade@gmail.com>
2025-02-27 22:09:14 -0300

    Delete TrabalhoIntrodutorio/12625142 (13) directory

--- TrabalhoIntrodutorio/12625142 (13)/FuncoesCabecalho.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FUNCOESCABACALHO_H__
-#define __FUNCOESCABECALHO_H__
-#include "cabeca.h"
-
-RegistroCabecalho *CriaStructCabecalho();
-void InicializaCabecalho(RegistroCabecalho *RegCab, FILE *arq);
-RegistroCabecalho *LeituraCabecalho(RegistroCabecalho*RegCab, FILE *arq);
-void ContadorTecnologias(RegistroCabecalho *RegCab,char ***Origem,char ***Destino,char *tecOrigem,char *tecDestino,int *n);
-#endif
